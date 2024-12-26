16bcb05e150f634911e1537b6e89453441c6e404
ZZTKDFO-PC\zztkdfo <zztkdfo@gmail.com>
2024-12-26 22:42:26 +0900

    Feat: 이미지 추가

---
+++ components/sections/data/images.ts
@@ -0,0 +1,82 @@
+export const PROFILE_IMAGES = [
+  {
+    id: 1,
+    src: "https://camo.githubusercontent.com/a5e9c18e7f1a0431fd04228a283f222f87ff0c7423c071ab6afb7ce86ce4be9c/68747470733a2f2f6f63746f6465782e6769746875622e636f6d2f696d616765732f68756c615f6c6f6f705f6f63746f64657830332e676966",
+    alt: "코딩 이미지 1",
+  },
+  {
+    id: 2,
+    src: "https://camo.githubusercontent.com/190e7d3bb2ff91e8d67d7ddddf458fede09c5f391dc0e66c290c2bb9e84106fa/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f38333648694a633770677a7938694e58436e2f67697068792e676966",
+    alt: "코딩 이미지 2",
+  },
+  {
+    id: 3,
+    src: "https://raw.githubusercontent.com/rahul-jha98/rahul-jha98/main/techstack.gif",
+    alt: "코딩 이미지 3",
+  },
+  {
+    id: 4,
+    src: "https://github.com/user-attachments/assets/b5c566cb-7847-42b9-ab6d-582730407718",
+    alt: "코딩 이미지 4",
+  },
+  {
+    id: 5,
+    src: "https://github.com/user-attachments/assets/07e32e7c-2865-4593-a77f-383db0a0ba0e",
+    alt: "코딩 이미지 5",
+  },
+  {
+    id: 6,
+    src: "https://github.com/user-attachments/assets/eb7ea291-99db-4225-b84b-2d597dd20370",
+    alt: "코딩 이미지 6",
+  },
+  {
+    id: 7,
+    src: "https://github.com/user-attachments/assets/2615a536-0529-41f0-9f0c-101ee192f224",
+    alt: "코딩 이미지 7",
+  },
+  {
+    id: 8,
+    src: "https://github.com/user-attachments/assets/44a33e3c-8668-4ad5-ab36-b3b40e7e6f18",
+    alt: "코딩 이미지 8",
+  },
+  {
+    id: 9,
+    src: "https://github.com/user-attachments/assets/5d7fb9ab-be66-40c9-83a1-2f2640e6f4cb",
+    alt: "코딩 이미지 9",
+  },
+  {
+    id: 10,
+    src: "https://github.com/user-attachments/assets/cfd0096e-28ef-4e47-99f9-148205bec553",
+    alt: "코딩 이미지 10",
+  },
+  {
+    id: 11,
+    src: "https://github.githubassets.com/images/mona-whisper.gif",
+    alt: "코딩 이미지 11",
+  },
+  {
+    id: 12,
+    src: "https://images.squarespace-cdn.com/content/v1/5769fc401b631bab1addb2ab/1541580611624-TE64QGKRJG8SWAIUS7NS/ke17ZwdGBToddI8pDm48kPoswlzjSVMM-SxOp7CV59BZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PI6FXy8c9PWtBlqAVlUS5izpdcIXDZqDYvprRqZ29Pw0o/coding-freak.gif",
+    alt: "코딩 이미지 12",
+  },
+  {
+    id: 13,
+    src: "https://user-images.githubusercontent.com/74038190/229223263-cf2e4b07-2615-4f87-9c38-e37600f8381a.gif",
+    alt: "코딩하는 고양이",
+  },
+  {
+    id: 14,
+    src: "https://user-images.githubusercontent.com/74038190/235224431-e8c8c12e-6826-47f1-89fb-2ddad83b3abf.gif",
+    alt: "깃허브 고양이",
+  },
+  {
+    id: 15,
+    src: "https://user-images.githubusercontent.com/74038190/212284158-e840e285-664b-44d7-b79b-e264b5e54825.gif",
+    alt: "코딩하는 개발자",
+  },
+  {
+    id: 16,
+    src: "https://user-images.githubusercontent.com/74038190/212284087-bbe7e430-757e-4901-90bf-4cd2ce3e1852.gif",
+    alt: "프로그래밍 언어 아이콘",
+  },
+];
