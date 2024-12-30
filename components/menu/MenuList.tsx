6124634a9600e79fcc48b53cb8a41b1128770326
ZZTKDFO-PC\zztkdfo <zztkdfo@gmail.com>
2024-12-31 01:59:24 +0900

    Feat: 섹션 추가 버튼 제거

--- components/menu/MenuList.tsx
+++ components/menu/MenuList.tsx
@@ -70,12 +70,12 @@ export default function MenuList({
       </DragDropContext>
 
       {/* 섹션 추가 버튼 */}
-      <button
+      {/* <button
         onClick={handleAddSection}
         className="w-full mt-4 p-3 text-gray-300 border border-dashed border-gray-600 rounded-md hover:border-gray-400 transition-colors duration-150 flex items-center justify-center gap-2"
       >
         <span>+ 섹션</span>
-      </button>
+      </button> */}
     </nav>
   );
 }
