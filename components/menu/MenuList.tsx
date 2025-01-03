d330e24c65bd800d53d22965c1a8c3c7b54e189c
ZZTKDFO-PC\zztkdfo <zztkdfo@gmail.com>
2025-01-03 17:50:03 +0900

    Fix: build 오류

--- components/menu/MenuList.tsx
+++ components/menu/MenuList.tsx
@@ -31,7 +31,7 @@ export default function MenuList({
   handleEdit,
   handleDelete,
   handleDragEnd,
-  handleAddSection,
+  // handleAddSection,
   handleEditingTitleChange,
   handleEditingIdChange,
   handleActiveMenuChange,
